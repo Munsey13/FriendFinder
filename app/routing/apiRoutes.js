443588ec82539e37d68bb34a822c58efed28bb3f
MM13 <Fastsoccer13@gmail.com>
2020-02-15 12:16:40 -0500

    set up app.post code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,37 @@ app.get("/api/friends", function (req, res){
 });
 
 app.post("/api/friends", function(req, res){
+    let checking = 0;
+    let bestMatch;
+    let score = 100;
 
-})
+    userData = req.body;    
+    userData.scores = userData.scores.map(function(item) {
+        return parseInt(item, 10);
+});
+
+sum = userData.scores.reduce((a, b) => a + b, 0);
+ 
+for (let i = 0 ; i < friendsData.length ; i++) {
+
+    let bfriendScore = friendsData[i].scores.reduce((a, b) => a + b, 0);
+    let totalDifference = Math.abs(sum - bfriendScore);
+
+    if (totalDifference <= score) {
+        bestMatch = friendsData[i];
+        score = totalDifference;
+    }  
+
+    checking++
+    console.log(checking)
+};
+
+if (friendsData.length = checking) {
+    friendsData.push(userData);
+    console.log(friendsData)
+    res.json(bestMatch)
 }
+
+});
+
+};
